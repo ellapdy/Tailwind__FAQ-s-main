992b4f94d462bfbb95c90d63da9e51dd436f567a
ellapdy <ellapadayao26@gmail.com>
2024-03-23 17:12:52 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        fblue: "#3b5896",
+        lblue: "#1573e5",
+        lgreen: "#008542",
+        ygreen: "#27941e",
+        dark: "#000000",
+        light: "#ffffff",
+        lgrey: "#f2f2f2",
+        dgrey: "#6b7280",
+        dyellow: "#d9a625",
+      },
+    },
+  },
+  plugins: [],
+}
+
